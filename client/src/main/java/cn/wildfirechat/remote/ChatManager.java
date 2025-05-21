3371a402c1020c2d996ed4e59368ca7f795d8303
qf5342 <qf5342@163.com>
2025-05-21 10:19:56 +0800

    Update ChatManager.java
    port

--- client/src/main/java/cn/wildfirechat/remote/ChatManager.java
+++ client/src/main/java/cn/wildfirechat/remote/ChatManager.java
@@ -234,7 +234,7 @@ public class ChatManager {
 
     private int backupAddressStrategy = 0;
     private String backupAddressHost = null;
-    private int backupAddressPort = 81;
+    private int backupAddressPort = 80;
     private String protoUserAgent = null;
     private final Map<String, String> protoHttpHeaderMap = new ConcurrentHashMap<>();
 
@@ -7821,14 +7821,14 @@ public class ChatManager {
     // shortlink port
     public int getPort() {
         if (!checkRemoteService()) {
-            return 81;
+            return 80;
         }
 
         try {
             return mClient.getPort();
         } catch (RemoteException e) {
             e.printStackTrace();
-            return 81;
+            return 80;
         }
     }
 
